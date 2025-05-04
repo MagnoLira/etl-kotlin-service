aecf2cea13f389da3e21da6afa748472244f8465
Magno Lira <silvalira968@gmail.com>
2025-05-04 20:04:44 -0300

    Enviroment test

---
+++ src/test/kotlin/com/magno/etlservice/EtlserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.magno.etlservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EtlserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
